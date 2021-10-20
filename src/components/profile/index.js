7338aa5cdce73f04906990ce54e95ba809e6c91b
DAVIDREACTS <91912539+DAVIDREACTS@users.noreply.github.com>
2021-10-20 12:47:46 -0400

    Update index.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,7 +2,7 @@ import React, { useReducer, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
 import Photos from './photos';
-import { getUserPhotosByUsername } from '../../services/firebase';
+import { getUserPhotosByUserId } from '../../services/firebase';
 
 
 export default function Profile({user}) {
@@ -21,8 +21,8 @@ export default function Profile({user}) {
     useEffect(() => {
         async function getProfileInfoAndPhotos() {
             
-            //const [user] = await getUserByUsername(user.username);
-            const photos = await getUserPhotosByUsername(user.userId);
+            //const [user] = await getUserByUserId(user.username);
+            const photos = await getUserPhotosByUserId(user.userId);
             dispatch({ profile: user, photosCollection: photos, followerCount: user.followers.length}); //dispatch allows to set values
         }
             getProfileInfoAndPhotos();
